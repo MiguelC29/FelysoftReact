e47b279579a2ef02a634c15203c215db8f5c8659
PC <tinitosguerrero@gmail.com>
2024-03-18 22:19:43 -0500

    METIENDOLE DURO

---
+++ src/components/PasswordStrength.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./styles.css";
+
+export const RegistrationForm = () => { 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (value) => {
+    setPassword(value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Name:", name);
+    console.log("Email:", email);
+    console.log("Password:", password);
+   
+  };
+
+  return (
+    <div className="page">
+      <div className="login-card">
+        <img src={logo} alt="Logo" />
+        <h2>Registro de Usuario</h2>
+        <form className="login-form" onSubmit={handleSubmit}>
+          <div className="username">
+            <input
+              autoComplete="off"
+              spellCheck="false"
+              className="control"
+              type="text"
+              placeholder="Name"
+              value={name}
+              onChange={handleNameChange}
+            />
+            <div id="spinner" className="spinner"></div>
+          </div>
+          <div className="username">
+            <input
+              autoComplete="off"
+              spellCheck="false"
+              className="control"
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={handleEmailChange}
+            />
+            <div id="spinner" className="spinner"></div>
+          </div>
+          <input
+            name="password"
+            spellCheck="false"
+            className="control"
+            type="password"
+            placeholder="Password"
+            onChange={(event) => handlePasswordChange(event.target.value)}
+          />
+          <button className="control" type="submit">
+            Regístrate
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
