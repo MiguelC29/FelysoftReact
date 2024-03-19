b29223d05e1666f3b3d1af07b10e32951d538584
PC <tinitosguerrero@gmail.com>
2024-03-19 16:49:57 -0500

    SE MODIFICARON VARIAS COSITAS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { RegistrationForm } from './components/PasswordStrength'; 
-
+import { RegistrationForm } from './components/PasswordStrength';
+import { UserList } from './components/Usuarios'; 
 import reportWebVitals from './reportWebVitals';
+import { App } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
