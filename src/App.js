a27bf50cdc82bfae54afb39637b558ca6131f2e3
MiguelC29 <miguelito28c@gmail.com>
2024-03-26 18:26:23 -0500

    borrando antigua datatable products

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Categories from './pages/Categories';
 import Roles from './pages/Roles';
 import Products from './pages/Products';
-import Prod from './pages/Prod';
 
 export default function App() {
   return (
@@ -11,7 +10,6 @@ export default function App() {
         <Route path='/categories' element={<Categories />} />
         <Route path='/roles' element={<Roles />} />
         <Route path='/productos' element={<Products />} />
-        <Route path='/products' element={<Prod />} />
       </Routes>
     </BrowserRouter>
   );
