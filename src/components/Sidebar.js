6d31ab641660ddbb23dacde66095cccac4956d1e
MiguelC29 <miguelito28c@gmail.com>
2024-03-31 20:20:21 -0500

    config sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -95,6 +95,13 @@ const NavHeader = () => (
         <button type="button" id="buttonsSide">
             <Icon icon="menu" />
         </button>
+        <div class="user">
+            <img src="https://i.postimg.cc/HLH1VGmw/user.png" alt="Foto de perfil"></img>
+            <div class="name">
+                <h5>GAES 3</h5>
+                <span>Admin</span>
+            </div>
+        </div>
     </header>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import './css/flags.css';
-import { RegistrationForm } from './components/PasswordStrength';
-import { UserList } from './components/Usuarios'; 
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
@@ -13,7 +12,6 @@ import { PrimeReactProvider } from 'primereact/api';
 import 'primeflex/primeflex.css';  
 import 'primereact/resources/primereact.css';
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
-import { App } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -24,4 +22,7 @@ root.render(
   </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
