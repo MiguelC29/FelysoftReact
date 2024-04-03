2142d87602977097457a12ecaef6e0a7d4e2a04e
MiguelC29 <miguelito28c@gmail.com>
2024-04-03 07:34:28 -0500

    re-design login

--- src/components/IniciarSesion.js
+++ src/components/IniciarSesion.js
@@ -4,10 +4,12 @@ import logo from "../img/logo.svg";
 export const InicioSesion = () => {
   return (
     <div className="page">
-      <img className="logo" src={logo} alt="Logo"/>
-      <div className="login-card"> 
+      <div className="container-logo bg-white p-3" style={{borderRadius: '60px 0 0 60px'}}>
+        <img className="logo" src={logo} alt="Logo" />
+      </div>
+      <div className="login-card" style={{borderRadius: '20px 80px 80px 20px'}}>
         <div className="content">
-      <h2>Iniciar Sesión</h2>
+          <h2>Iniciar Sesión</h2>
           <form className="login-form">
             <div className="input__wrapper">
               <input
@@ -22,21 +24,21 @@ export const InicioSesion = () => {
                 Email
               </label>
               <div className="input__wrapper">
-              <input
-                id="password"
-                type="password"
-                name="password"
-                placeholder="Password"
-                required
-                className="input__field"
-              />
-              <label htmlFor="password" className="input__label">
-                Password
-              </label>
+                <input
+                  id="password"
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  required
+                  className="input__field"
+                />
+                <label htmlFor="password" className="input__label">
+                  Password
+                </label>
               </div>
               <div id="spinner" className="spinner"></div>
             </div>
-            
+
             <button className="control" type="button">
               INICIAR SESIÓN
             </button>
