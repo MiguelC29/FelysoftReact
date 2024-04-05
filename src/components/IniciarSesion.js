982eecb5121f06206d96375994f9bb626e771f37
devsan-g3 <santimahe001@gmail.com>
2024-04-05 00:24:55 -0500

    login security

--- src/components/IniciarSesion.js
+++ src/components/IniciarSesion.js
@@ -1,9 +1,40 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom'; // Importa useNavigate
 import "../css/inicioSesion.css";
 import logo from "../img/logo.svg";
 
-
 export const InicioSesion = () => {
+  const navigate = useNavigate(); // Obtiene la función de navegación
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const response = await fetch('http://localhost:8086/api/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username: email, password: password }),
+      });
+      const data = await response.json();
+      setLoading(false);
+      if (response.ok) {
+        localStorage.setItem('token', data.token);
+        navigate('/productos'); // Navega a la página de productos
+      } else {
+        alert(data.message);
+      }
+    } catch (error) {
+      console.error('Error al iniciar sesión:', error);
+      setLoading(false);
+      alert('Error al iniciar sesión. Por favor, inténtalo de nuevo más tarde.');
+    }
+  };
+
   return (
     <div className="page">
       <div className="container-logo bg-white p-3" style={{borderRadius: '60px 0 0 60px'}}>
@@ -12,18 +43,20 @@ export const InicioSesion = () => {
       <div className="login-card" style={{borderRadius: '20px 80px 80px 20px'}}>
         <div className="content">
           <h2>Iniciar Sesión</h2>
-          <form className="login-form" style={{background: '#19191a'}}>
+          <form className="login-form" onSubmit={handleSubmit} style={{background: '#19191a'}}>
             <div className="input__wrapper">
               <input
                 autoComplete="off"
                 spellCheck="false"
-                type="email"
-                placeholder="Email"
+                type="Username"
+                placeholder="Username"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 required
                 className="input__field"
               />
-              <label htmlFor="email" className="input__label">
-                Email
+              <label htmlFor="Username" className="input__label">
+                Username
               </label>
               <div className="input__wrapper">
                 <input
@@ -31,6 +64,8 @@ export const InicioSesion = () => {
                   type="password"
                   name="password"
                   placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                   required
                   className="input__field"
                 />
@@ -41,8 +76,8 @@ export const InicioSesion = () => {
               <div id="spinner" className="spinner"></div>
             </div>
 
-            <button className="control" type="button">
-              <Link to={'/productos'} className='text-white text-decoration-none'>INICIAR SESIÓN</Link>
+            <button className="control" type="submit" disabled={loading}>
+              {loading ? 'Cargando...' : 'INICIAR SESIÓN'}
             </button>
           </form>
         </div>
