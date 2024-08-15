5ef43d5d078c6f4a8bf16c08ff0383f1ffe450af
Andre391 <yullysoto24@gmail.com>
2024-08-14 19:19:46 -0500

    Arreglo del Perfil de Usuario

--- src/components/userspage/ProfilePage.jsx
+++ src/components/userspage/ProfilePage.jsx
@@ -20,22 +20,25 @@ function ProfilePage() {
             console.error('Error fetching profile information:', error);
         }
     }
-    const roles = [
-        "ADMINISTRATOR",
-        "INVENTORY_MANAGER",
-        "FINANCIAL_MANAGER",
-        "SALESPERSON"
-    ];
+    const getGenderIcon = (gender) => {
+        if (gender === "MASCULINO") {
+            return <i className="fas fa-mars"></i>;
+        } else if (gender === "FEMENINO") {
+            return <i className="fas fa-venus"></i>;
+        } else {
+            return <i className="fas fa-genderless"></i>;
+        }
+    };
     const formatDate = (dateString) => {
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
         return new Date(dateString).toLocaleDateString('es-ES', options);
     };
     const description = (role) => {
         const roleDescriptions = {
-            "ADMINISTRATOR": "Administrador del Software Felysoft.",
-            "INVENTORY_MANAGER": "Gerente de Inventario del Software Felysoft.",
-            "FINANCIAL_MANAGER": "Gerente de Finanzas del Software Felysoft.",
-            "SALESPERSON": "Vendedor del Software Felysoft.",
+            "ADMINISTRATOR": "Administrador de Felysoft.",
+            "INVENTORY_MANAGER": "Gerente de Inventario de Felysoft.",
+            "FINANCIAL_MANAGER": "Gerente de Finanzas de Felysoft.",
+            "SALESPERSON": "Vendedor de Felysoft.",
             "CUSTOMER": "Bienvenido A Su Perfil."
         };
         return roleDescriptions[role] || "USER";
@@ -48,14 +51,14 @@ function ProfilePage() {
                     <div className="perfil-usuario-portada">
                         <div className="perfil-usuario-avatar">
                             {profileInfo.image ?
-                            <img id='imagen-perfil'
-                                src={`data:${profileInfo.imageType};base64,${profileInfo.image}`}
-                                alt={`Imagen usuario ${profileInfo.names}`}
-                            /> : 
-                            <img id='imagen-perfil'
-                                src="https://st4.depositphotos.com/4329009/19956/v/450/depositphotos_199564354-stock-illustration-creative-vector-illustration-default-avatar.jpg"
-                                alt={`No cuenta con img de perfil`}
-                            />}
+                                <img id='imagen-perfil'
+                                    src={`data:${profileInfo.imageType};base64,${profileInfo.image}`}
+                                    alt={`Imagen usuario ${profileInfo.names}`}
+                                /> :
+                                <img id='imagen-perfil'
+                                    src="https://st4.depositphotos.com/4329009/19956/v/450/depositphotos_199564354-stock-illustration-creative-vector-illustration-default-avatar.jpg"
+                                    alt={`No cuenta con img de perfil`}
+                                />}
                             <button type="button" className="boton-avatar">
                                 <i className="far fa-image"></i>
                             </button>
@@ -67,7 +70,7 @@ function ProfilePage() {
                 </div>
                 <div className="perfil-usuario-body">
                     <div className="perfil-usuario-bio">
-                        <h3 className="titulo">{profileInfo.names} {profileInfo.lastNames}</h3>
+                        <h3 className="titulo">{profileInfo.names} {profileInfo.lastNames} {getGenderIcon(profileInfo.gender)}</h3>
                         <p className="texto">
                             <h3>{description(profileInfo.role)}</h3>
                         </p>
@@ -76,23 +79,11 @@ function ProfilePage() {
                         <ul className="lista-datos">
                             <li><i className="icono fas fa-envelope"></i> Correo Electrónico: {profileInfo.email}</li>
                             <li><i className="icono fas fa-user"></i> Nombre de Usuario: {profileInfo.user_name}</li>
-                            {roles.includes(profileInfo.role) && (
-                                <li><i className="icono fas fa-briefcase"></i> Trabaja en: Felysoft</li>
-                            )}
-                            <li>
-                                <i className="icono fas fa-building"></i> Cargo: {
-                                    {
-                                        "ADMINISTRATOR": "Administrador",
-                                        "INVENTORY_MANAGER": "Gestor de Inventario",
-                                        "FINANCIAL_MANAGER": "Gestor Financiero",
-                                        "SALESPERSON": "Vendedor",
-                                        "CUSTOMER": "Cliente"
-                                    }[profileInfo.role] || profileInfo.role
-                                }
-                            </li>
+                            <li><i className="icono fas fa-phone"></i> Numero de Contacto: {profileInfo.phoneNumber}</li>
+                            <li><i className="icono fas fa-map-marker-alt"></i> Dirección: {profileInfo.address}</li>
                         </ul>
                         <ul className="lista-datos">
-                            <li><i className="icono fas fa-map-marker-alt"></i> Dirección: {profileInfo.address}</li>
+                            <li><i className="icono fas fa-solid fa-passport"></i> Tipo Documento: {profileInfo.typeDoc}</li>
                             <li><i className="icono fas fa-regular fa-address-card"></i> Identificación: {profileInfo.numIdentification}</li>
                             <li><i className="icono fas fa-user-check"></i> Registro: {formatDate(profileInfo.dateRegister)} </li>
                         </ul>
